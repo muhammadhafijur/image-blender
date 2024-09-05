ffaf5247dcf973a92499e19b2bf2344867c070af
MuhammadHafijur <85216180+MuhammadHafijur@users.noreply.github.com>
2024-09-05 16:50:12 +0600

    :recycle: fix: Fix ts error

--- src/main.ts
+++ src/main.ts
@@ -20,20 +20,22 @@ const GA_MEASUREMENT_ID = import.meta.env.VITE_GA_MEASUREMENT_ID;
 
 
 function loadGoogleAnalytics() {
-  const script = document.createElement('script');
-  script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
-  script.async = true;
-  document.head.appendChild(script);
-
-  script.onload = () => {
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', GA_MEASUREMENT_ID);
-  };
-}
-
-loadGoogleAnalytics();
+    const script = document.createElement('script');
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+    script.async = true;
+    document.head.appendChild(script);
+  
+    script.onload = () => {
+      (window as any).dataLayer = (window as any).dataLayer || [];
+      function gtag(...args: any[]) {
+        (window as any).dataLayer.push(args);
+      }
+      gtag('js', new Date());
+      gtag('config', GA_MEASUREMENT_ID);
+    };
+  }
+  
+  loadGoogleAnalytics();
 
 
 
