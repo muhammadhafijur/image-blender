c53668ea9b4a62ab8afe4cd0228d83b0cde196b4
subhamc88 <subham020304@gmail.com>
2024-10-07 16:55:07 -0400

    updated TheHeader.vue for github icon
    Added gradient border to github icons in navbar.

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -22,20 +22,27 @@
 
       <div class="flex flex-1 items-center justify-end">
         <div class="flex items-center gap-4">
-          <a
-            href="https://github.com/muhammadhafijur/image-blender"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="w-8 h-8 sm:h-10 sm:w-10 grid place-items-center bg-gray-200 dark:bg-gray-100 rounded-md"
-            ><svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="size-4 sm:size-5"
+          <div class="p-1 rounded-lg transition-all duration-500 ease-in-out hover:bg-gradient-to-r hover:from-blue-500 hover:via-blue-500 hover:via-20% hover:to-purple-500 hover:to-80%">
+            <a
+              href="https://github.com/muhammadhafijur/image-blender"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="w-8 h-8 sm:h-10 sm:w-10 grid place-items-center bg-gray-200 dark:bg-gray-100 rounded-md"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="size-4 sm:size-5"
+              >
+                <path
+                  d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"
+                ></path>
+                <path d="M9 18c-4.51 2-5-2-7-2"></path></svg
+              ><span class="sr-only">GitHub</span></a
             >
               <path
                 d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"
