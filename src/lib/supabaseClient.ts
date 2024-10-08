ed6a3845bf1a71ac1301e874fc24776f38b9b371
Adebesin Cell <adebesintolulope80@gmail.com>
2024-10-08 04:16:50 +0100

    ✨feat: Adds testimonial section - design cards, layout and added dummy data for testimonial reviews

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,13 +1,12 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-    throw new Error('Supabase URL or Key is missing');
-  }
+	throw new Error("Supabase URL or Key is missing");
+}
 
 const supabase = createClient(supabaseUrl, supabaseKey);
-
 
 export default supabase;
