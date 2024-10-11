9b68817b47bf8a3d6668e6e1818bf53609dffe03
MuhammadHafijur <85216180+MuhammadHafijur@users.noreply.github.com>
2024-10-11 12:06:30 +0600

    :art: styles: dark-mode: Improved dark mode

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="py-4">
+  <div class="py-4  dark:bg-zinc-950">
     <div
       class="relative max-w-screen-xl px-4 sm:px-6 md:px-8 mx-auto flex flex-col md:flex-row h-[50vh] gap-4 lg:h-[330px]"
     >
       <!-- bg-[#F5F5F7] -->
       <div
-        class="h-full flex-1 flex flex-col justify-center rounded-xl bg-[#F5F5F7]"
+        class="h-full flex-1 flex flex-col justify-center rounded-xl bg-[#F5F5F7] dark:bg-[#1d1d1d]"
       >
         <div class="px-4">
           <h1
@@ -16,13 +16,13 @@
           </h1>
           <div
             id="editor"
-            class="mt-2 md:mt-4 flex justify-start font-inter dark:bg-custom-dark-400"
+            class="mt-2 md:mt-4 flex justify-start font-inter "
           >
             <p
-              class="py-1 bg-white/90 px-4 text-start text-sm text-slate-500 dark:text-gray-300 dark:bg-slate-900 rounded-full"
+              class="py-1 bg-white/90 px-4 text-start text-sm text-slate-500 dark:text-gray-300 dark:bg-slate-800 rounded-full"
             >
               Over
-              <span class="text-emerald-500 dark:text-gray-100 font-semibold">{{
+              <span class="text-emerald-500 font-semibold">{{
                 downloadCount - 1
               }}</span>
               images downloaded so far!
@@ -64,11 +64,11 @@
       </div>
 
       <button
-        class="h-16 w-16 md:h-28 md:w-28 absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 bg-white grid place-items-center rounded-full"
+        class="h-16 w-16 md:h-28 md:w-28 absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 bg-white dark:bg-zinc-950 grid place-items-center rounded-full"
       >
         <a
           href="#editor"
-          class="h-14 w-14 md:h-24 md:w-24 rounded-full bg-indigo-500 flex items-center justify-center text-[10px] md:text-base font-semibold hover:bg-indigo-600 transition-colors duration-300 text-white text-center"
+          class="h-14 w-14 md:h-24 md:w-24 rounded-full bg-indigo-500 dark:bg-emerald-500 flex items-center justify-center text-[10px] md:text-base font-semibold hover:bg-indigo-600 transition-colors duration-300 text-white text-center"
         >
           Start <br />
           Blending
